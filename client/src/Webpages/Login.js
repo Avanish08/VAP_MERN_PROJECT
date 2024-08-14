9dec22731441fe25a665ab9e10f43597e4d84726
Avanish08 <ojhaavanish190@gmail.com>
2024-08-14 14:31:13 +0530

    second1.1 commit

---
+++ client/src/Webpages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const login = () => {
+  return (
+  <>
+  </>
+  )
+}
+
+export default login
--- client/src/Webpages/Rejs.js
+++ client/src/Webpages/Rejs.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
-export default function Rejs() {
-  return (
-    <>
-    <form action="/" className='bg-sky-500'>
-    <label htmlFor="name">
-        NAME
-    </label>
-    <input type="text" name="name" id="name" /> <br />
-    <label htmlFor="password"> Password</label>
-    <input type="password" name="password" id="pasword" /><br/>
-    </form>
-    </>
-  )
+const Rejs = () => {
+  
+    return (
+        <>
+        <form action="/" className='bg-sky-500'>
+        <label htmlFor="name">
+            NAME
+        </label>
+        <input type="text" name="name" id="name" /> <br />
+        <label htmlFor="password"> Password</label>
+        <input type="password" name="password" id="pasword" /><br/>
+        </form>
+        </>
+      )
+  
 }
+
+export default Rejs
+
